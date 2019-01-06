508012a145a4480436576580b40b21ad1b16ddd8
jynnie <jynnie@mit.edu>
2019-01-06 15:05:39 -0500

    process port for heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const pug = require("pug");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 // Setting up Views and Directories
 app.set("views", path.join(__dirname, "views"));
